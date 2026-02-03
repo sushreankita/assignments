0b66fc3bee1f2baf28b04604df4e5eeadab24683
sushreankita <sushreeankita47@gmail.com>
2026-02-03 22:23:44 +0530

    added

---
+++ daynotoname.py
@@ -0,0 +1,17 @@
+n=int(input('enter day no'))
+if n == 0:
+    print('monday')
+elif n == 1:
+    print('tuesday')
+elif n == 2:
+    print('wednesday')
+elif n == 3:
+    print('thursday')
+elif n == 4:
+    print('friday')
+elif  n == 5:
+    print('saturday')
+elif n == 6:
+    print('sunday')
+else:
+    print('invalid day no')
